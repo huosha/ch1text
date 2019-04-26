7c398b6894e1f599f8bcee0789730efe3f9977d3
huosha <35130874+huosha@users.noreply.github.com>
2019-04-26 14:24:19 +0800

    Student新增imagePath

--- src/main/java/com/bee/sample/ch1/model/Student.java
+++ src/main/java/com/bee/sample/ch1/model/Student.java
@@ -1,27 +1,43 @@
 package com.bee.sample.ch1.model;
 
+import java.util.Objects;
+
 public class Student {
     private String name;
     private String id;
     private int sex;
+    private String imagePath;
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public int getSex() {
         return sex;
     }
+
     public void setSex(int sex) {
         this.sex = sex;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 
     @Override
@@ -30,6 +46,23 @@ public class Student {
                 "name='" + name + '\'' +
                 ", id='" + id + '\'' +
                 ", sex=" + sex +
+                ", imagePath='" + imagePath + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return sex == student.sex &&
+                name.equals(student.name) &&
+                id.equals(student.id) &&
+                imagePath.equals(student.imagePath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, sex, imagePath);
     }
 }
