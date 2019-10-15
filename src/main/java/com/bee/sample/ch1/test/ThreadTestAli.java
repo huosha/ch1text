6fd67e245e52c0223e64d64549b2bef69eb79057
jiangcan0502@163.com <java2018@>
2019-10-15 23:56:55 +0800

    多线程之间的通信aliali

--- src/main/java/com/bee/sample/ch1/test/ThreadTestAli.java
+++ src/main/java/com/bee/sample/ch1/test/ThreadTestAli.java
@@ -1,42 +1,77 @@
 package com.bee.sample.ch1.test;
 
 public class ThreadTestAli {
+
+    static final Object str1 = new Object();
+
+    private static int count = 0;
+
     public static void main(String[] args) {
-        A a = new A();
-        Thread threadA = new Thread(a);
-        B b = new B();
-        Thread threadB = new Thread(b);
-        C c = new C();
-        Thread threadC = new Thread(c);
-        threadA.start();
-        threadB.start();
-        threadC.start();
+        new Thread(() -> {
+            while (true) {
+                synchronized (str1) {
+                    if (count % 3 == 0) {
+                        System.out.print("a");
+                        count++;
+                        if (count == 304) {
+                            break;
+                        }
+                        str1.notifyAll();
+                    } else {
+                        try {
+                            str1.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                synchronized (str1) {
+                    if (count % 3 == 1) {
+                        System.out.print("l");
+                        count++;
+                        str1.notifyAll();
+                        if (count == 305) {
+                            break;
+                        }
+                    } else {
+                        try {
+                            str1.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                synchronized (str1) {
+                    if (count % 3 == 2) {
+                        System.out.print("i,");
+                        count++;
+                        if (count == 306) {
+                            break;
+                        }
+                        str1.notifyAll();
+                    } else {
+                        try {
+                            str1.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        }).start();
+
     }
 }
 
-class A implements Runnable {
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 101; i++)
-        System.out.print("a");
-    }
-}
-class B implements Runnable {
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 101; i++) {
-            System.out.print("l");
-        }
-
-    }
-}
-class C implements Runnable {
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 101; i++)
-        System.out.print("i");
-    }
-}
